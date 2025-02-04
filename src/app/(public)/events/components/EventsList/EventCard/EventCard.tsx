613a2a1eac311c252469ac32b5d8483fbfd2bc58
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2025-02-04 16:56:42 -0500

    fixing spaces and layout

--- src/app/(public)/events/components/EventsList/EventCard/EventCard.tsx
+++ src/app/(public)/events/components/EventsList/EventCard/EventCard.tsx
@@ -18,7 +18,7 @@ export default function EventCard({ event }: Props) {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="bg-white rounded-xl w-[clamp(280px, 20%, 300px)] shadow-lg hover:shadow-2xl transform transition duration-300 hover:scale-105 flex flex-col items-center p-4 gap-4"
+      className="bg-white rounded-xl w-[clamp(280px, 20%, 300px)] border-2 shadow-lg hover:shadow-2xl transform transition duration-300 hover:scale-105 flex flex-col items-center pt-4 pb-0 pl-0 pr-0 gap-4"
     >
       <div className="relative flex-shrink-0">
         <div className="absolute inset-0 rounded-full"></div>
@@ -28,19 +28,19 @@ export default function EventCard({ event }: Props) {
       </div>
       <div className="flex flex-cols h-full">
         <div>
-          <h2 className="text-xl font-bold text-black mb-2 whitespace-normal break-words">
+          <h2 className="text-xl font-bold text-black ml-4 mr-4 mb-2 whitespace-normal break-words">
             {event.nombre_evento}
           </h2>
-          <div className="flex items-center mb-4 text-gray-600">
+          <div className="flex items-center mr-4 font-bold  text-gray-600">
             <CalendarIcon className="h-5 w-5 text-blue-500 mr-2" />
-            <p className="text-sm">{event.fecha}</p>
+            <p className="text-base">{event.fecha}</p>
           </div>
-          <div className="flex items-center text-gray-600">
+          <div className="flex items-center mr-4 font-bold  text-gray-600">
             <MapPinIcon className="h-5 w-5 text-red-500 mr-2" />
-            <p className="text-sm truncate">{event.lugar}</p>
+            <p className="text-base truncate">{event.lugar}</p>
           </div>
-          <div className="flex items-center justify-center mt-4">
-            <Button className="bg-primary hover:bg-primary-dark gradient-btn text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+          <div>
+            <Button className="m-0 mt-4 w-full bg-primary hover:bg-primary-dark gradient-btn text-white font-bold transition duration-300 ease-in-out transform hover:text-lg rounded-t-none">
               <Link href={routes.event(event.id.toString()).path}>Ver detalles</Link>
             </Button>
           </div>

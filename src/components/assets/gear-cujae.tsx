876890d43d1d29b2451cc3b475d75591f1bab940
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2025-02-05 02:10:44 -0500

    white background

--- src/components/assets/gear-cujae.tsx
+++ src/components/assets/gear-cujae.tsx
@@ -6,10 +6,10 @@ export default function GearCujae() {
     <>
       <p>probando imagen</p>
       <Image
-        src='/images/ai.gif'
+        src='/images/ai2.gif'
         width={130}
         height={220}
-        style={{ width: '100px', height: '220px' }}
+        style={{ width: '200px', height: '420px', zIndex: -1, top='4px'}}
         alt='se hizo el intento'
       />
     </>

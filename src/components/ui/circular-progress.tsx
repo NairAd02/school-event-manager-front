c4ef511f2252243d143d21571f2d208f6806383c
Adrian <adrian.suarez.alphen@gmail.com>
2025-02-04 22:48:24 -0500

    feat(app): added loaders

--- src/components/ui/circular-progress.tsx
+++ src/components/ui/circular-progress.tsx
@@ -9,7 +9,7 @@ export default function CircularProgress({ className = "" }: Props) {
     <div className="flex items-center justify-center h-64">
       <div
         className={
-          "animate-spin rounded-full border-t-2 border-b-2 border-secondary " +
+          "animate-spin rounded-full border-t-2 border-b-2 " +
           className
         }
       ></div>
